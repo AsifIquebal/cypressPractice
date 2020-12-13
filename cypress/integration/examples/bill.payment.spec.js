004b2893ed3a02909f7a1f6c4e1f12fec2da97bf
asus <asu9421@gmail.com>
2020-12-13 21:42:16 +0530

    updated

--- cypress/integration/examples/bill.payment.spec.js
+++ cypress/integration/examples/bill.payment.spec.js
@@ -1,17 +1,17 @@
 /// <reference types="cypress" />
- 
+
 import LoginPage from "../../page_objects/LoginPage"
 import BillPaymentsPage from "../../page_objects/BillPaymentsPage"
 import BasePage from "../../page_objects/BasePage"
- 
+
 const loginPage = new LoginPage
 const billPaymentsPage = new BillPaymentsPage
 const basePage = new BasePage
 
-describe("Bill Payment Test",()=>{
+describe("Bill Payment Test", () => {
 
-    it("Pay a Bill",()=>{
-        cy.fixture("user").then(function(user){
+    it("Pay a Bill", () => {
+        cy.fixture("userData").then(function (user) {
             cy.log('user: ' + user.userName)
             cy.log('user: ' + user.passWord)
             loginPage.login(user.userName, user.passWord)
